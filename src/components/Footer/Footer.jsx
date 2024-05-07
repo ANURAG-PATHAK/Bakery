179371c77a563d8392aef3b13095abc91a5e5772
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-05-07 17:05:36 +0530

    chore: Update Footer component with social media links and useful links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,55 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa6";
 import "./Footer.css";
+
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="left">
-        <h2 className="footer-heading">
-          <a href="">Bakery</a>
-        </h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-          dolorum sequi corporis facilis minima magni optio itaque et, atque
-          facere aut quae eligendi necessitatibus exercitationem veniam
-          consequatur? Voluptatibus, nobis illo.
-        </p>
+    <>
+      <div className="footer">
+        <div className="left">
+          <h2 className="footer-heading">
+            <a href="">Bakery</a>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis dolorum sequi corporis facilis minima magni optio
+            itaque et, atque facere aut quae eligendi necessitatibus
+            exercitationem veniam consequatur? Voluptatibus, nobis illo.
+          </p>
+          <div className="hero-socials">
+            <a href="">
+              <FaLinkedin />
+            </a>
+            <a href="">
+              <FaInstagram />
+            </a>
+            <a href="">
+              <FaFacebook />
+            </a>
+          </div>
+        </div>
+        <div className="middle">
+          <h3>Our Stores</h3>
+          <a href="">Slovakia</a>
+          <a href="">Czech Republic</a>
+          <a href="">Finland</a>
+          <a href="">Poland</a>
+        </div>
+        <div className="right">
+          <h3>Useful Links</h3>
+          <a href="">Home</a>
+          <a href="">Privacy Policy</a>
+          <a href="">Terms Condition</a>
+          <a href="">Contact Us</a>
+        </div>
       </div>
-      <div className="middle">
-        <h3>Our Stores</h3>
-        <a href="">Slokova</a>
-        <a href="">Shop</a>
-        <a href="">Services</a>
-        <a href="">Blog</a>
+      <div className="credits">
+        <div className="credits-content">
+          Designed by <a href="">ll cc</a> and developed by{" "}
+          <a href="">anuraag</a>
+        </div>
+        <div className="credits-copyright">© Bakery All rights reserved.</div>
       </div>
-      <div className="right"></div>
-    </div>
+    </>
   );
 };
 
